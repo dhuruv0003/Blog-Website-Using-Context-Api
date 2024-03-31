ed65b58a9deceea9bb23158dcba362622659b802
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-01 01:08:24 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+     <Header></Header>
+     <Blogs></Blogs>
+     <Pagination></Pagination>
+    </div>
+  )
+}
+
+export default App
