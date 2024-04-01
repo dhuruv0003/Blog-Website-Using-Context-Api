025877ff630ebfc90ec711a3ca09c6b4744f6fa6
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-04-02 00:28:04 +0530

    new commit

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,22 +7,22 @@ export default function Pagination() {
     return (
         <div className="p-2 rounded-s-full rounded-r-full bg-[#fffcdd] mb-4">
 
-            <div className=" flex justify-evenly">
+            <div className=" flex justify-evenly items-center">
                 {
                     //Previous buttoon tabhi ayega jab page>1 hoga
                     page > 1 &&
-                    <button className="border p-1 rounded-lg border-black" onClick={() => handlePageChange(page - 1)}>
+                    <button className="p-1 rounded-lg font-bold text-[#e7e0c4] bg-[#003049]" onClick={() => handlePageChange(page - 1)}>
                         Previous
                     </button>
                 }
 
                 <p>
-                    page {page} of {totalPages}
+                    Page {page} of {totalPages}
                 </p>
 
                 {
                     page < totalPages &&
-                    <button className="border p-1 rounded-lg border-black" onClick={() => handlePageChange(page + 1)}>Next</button>
+                    <button className="px-3 py-1 rounded-lg font-bold text-[#e7e0c4] bg-[#003049]" onClick={() => handlePageChange(page + 1)}>Next</button>
                 }
 
 
